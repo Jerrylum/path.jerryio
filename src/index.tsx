3723109f141343528840395c81563ba6ab4d8bf1
Jerrylum <serverofjerry@gmail.com>
2023-06-29 03:17:43 +0800

    :fire: Remove index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
