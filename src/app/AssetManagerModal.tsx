370538480e32e3d55306c5ac5683ade0461ab1d8
Jerrylum <serverofjerry@gmail.com>
2023-09-05 23:49:54 +0800

    :construction: Implement Asset Manager Modal (5)

--- src/app/AssetManagerModal.tsx
+++ src/app/AssetManagerModal.tsx
@@ -1,7 +1,23 @@
 import { observer } from "mobx-react-lite";
 import { Modal } from "../component/Modal";
 import { MuiFileInput } from "mui-file-input";
-import { Box, Card, Chip, IconButton, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  Chip,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Radio,
+  RadioGroup,
+  Typography
+} from "@mui/material";
 import { action, makeAutoObservable } from "mobx";
 import { FieldImageAsset, FieldImageOriginType, getFieldImageOriginTypeDescription } from "../core/Asset";
 import { getAppStores } from "../core/MainApp";
@@ -20,10 +36,21 @@ export const AssetManagerModalSymbol = Symbol("AssetManagerModalSymbol");
 class FieldImageManagerVariables {
   selected: FieldImageAsset<FieldImageOriginType> | null = null;
 
-  newName: string = "My Custom Field Image";
-  newHeightInMM: number = 100;
-  newUrl: string = "";
-  newUpload: File | null = null;
+  draft: {
+    name: string;
+    heightInMM: number;
+    url: string;
+    upload: File | null;
+  } | null = null;
+
+  newDraft() {
+    this.draft = {
+      name: "My Custom Field Image",
+      heightInMM: 100,
+      url: "",
+      upload: null
+    };
+  }
 
   constructor() {
     makeAutoObservable(this);
@@ -144,13 +171,186 @@ export const FieldImagePreview = observer((props: { preview: FieldImageAsset<Fie
   );
 });
 
-export const AssetManagerModal = observer(() => {
+export const FieldImageList = observer((props: { variables: FieldImageManagerVariables }) => {
+  const { assetManager } = getAppStores();
+
+  const { variables } = props;
+
+  return (
+    <Box>
+      <Box id="assets-list">
+        {/* Using the SVG viewBox solution to allow the use of min-height */}
+        <svg viewBox="0 0 0.6 0.4"></svg>
+        <Box id="assets-list-content">
+          <List dense>
+            {assetManager.assets.map(asset => (
+              <FieldImageAssetItem key={asset.signature} variables={variables} asset={asset} />
+            ))}
+          </List>
+        </Box>
+      </Box>
+      <Button
+        variant="contained"
+        color="primary"
+        disableElevation
+        sx={{ marginTop: "12px" }}
+        onClick={() => variables.newDraft()}>
+        Add
+      </Button>
+    </Box>
+  );
+});
+
+export const NewFieldImageForm = observer((props: { variables: FieldImageManagerVariables }) => {
+  const { assetManager } = getAppStores();
+
+  const { variables } = props;
+
+  const draft = variables.draft;
+  if (draft === null) return null;
+
+  return (
+    <Box>
+      {/* <Typography variant="h4" gutterBottom>
+        New image
+      </Typography> */}
+      <Box
+        sx={{
+          marginTop: "1em",
+          display: "flex",
+          gap: "12px",
+          flexWrap: "wrap",
+          alignItems: "center",
+          width: "100%"
+        }}>
+        <ObserverInput
+          label="Name"
+          getValue={() => draft.name}
+          setValue={(value: string) => (draft.name = value)}
+          isValidIntermediate={() => true}
+          isValidValue={value => value !== ""}
+          sx={{ flexGrow: 1 }}
+          onKeyDown={e => e.stopPropagation()}
+        />
+        <ObserverInput
+          label="Height (mm)"
+          getValue={() => draft.heightInMM + ""}
+          setValue={(value: string) => {
+            const setValue = parseFormula(value, NumberUOL.parse)!.compute(UnitOfLength.Millimeter);
+            draft.heightInMM = Math.max(100, setValue);
+          }}
+          isValidIntermediate={() => true}
+          isValidValue={(candidate: string) => parseFormula(candidate, NumberUOL.parse) !== null}
+          numeric
+          sx={{ width: "100px" }}
+          onKeyDown={e => e.stopPropagation()}
+        />
+      </Box>
+
+      {/* <Typography variant="h4" gutterBottom>
+        Source
+      </Typography> */}
+      {/* <Box
+        sx={{
+          marginTop: "1em",
+          display: "flex",
+          gap: "12px",
+          flexWrap: "nowrap",
+          alignItems: "center",
+          width: "100%"
+        }}>
+        <ObserverInput
+          label="URL"
+          getValue={() => draft.url}
+          setValue={(value: string) => (draft.url = value)}
+          isValidIntermediate={() => true}
+          isValidValue={value => value !== ""} // TODO
+          sx={{ flexGrow: 1 }}
+          onKeyDown={e => e.stopPropagation()}
+        />
+        <Typography variant="body1" sx={{}}>
+          or
+        </Typography>
+        <MuiFileInput
+          sx={{ flexGrow: 1 }}
+          placeholder="File Upload"
+          value={draft.upload}
+          onChange={action((file: File | null) => (draft.upload = file))}
+          size="small"
+        />
+      </Box> */}
+      <Box
+        sx={{
+          marginTop: "1em"
+        }}>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Source</FormLabel>
+          <RadioGroup aria-labelledby="demo-radio-buttons-group-label" defaultValue="" row name="radio-buttons-group">
+            <FormControlLabel
+              value="url"
+              control={<Radio />}
+              label={
+                <ObserverInput
+                  label="URL"
+                  getValue={() => draft.url}
+                  setValue={(value: string) => (draft.url = value)}
+                  isValidIntermediate={() => true}
+                  isValidValue={value => value !== ""} // TODO
+                  // sx={{ flexGrow: 1 }}
+                  onKeyDown={e => e.stopPropagation()}
+                />
+              }
+            />
+            <FormControlLabel
+              value="male"
+              control={<Radio />}
+              label={
+                <MuiFileInput
+                  // sx={{ flexGrow: 1 }}
+                  placeholder="File Upload"
+                  value={draft.upload}
+                  onChange={action((file: File | null) => (draft.upload = file))}
+                  size="small"
+                />
+              }
+            />
+          </RadioGroup>
+        </FormControl>
+
+        <Typography variant="body1" sx={{}}>
+          If the url is provided, the image will be downloaded and cached in your computer. The url will also be
+          included in the path file. The image will be automatically downloaded when the path file is opened on a
+          different computer.
+        </Typography>
+      </Box>
+    </Box>
+  );
+});
+
+export const FieldImageSection = observer(() => {
   const { assetManager } = getAppStores();
 
   const variables = useMobxStorage(() => new FieldImageManagerVariables(), []);
 
   const selected = variables.selected;
 
+  return (
+    <Box>
+      <Typography variant="h3" fontSize={18} gutterBottom>
+        Field Image
+      </Typography>
+      <Box id="field-image-assets">
+        <Box id="assets-list-add">
+          {!variables.draft && <FieldImageList variables={variables} />}
+          {variables.draft && <NewFieldImageForm variables={variables} />}
+        </Box>
+        {selected && <FieldImagePreview preview={selected} />}
+      </Box>
+    </Box>
+  );
+});
+
+export const AssetManagerModal = observer(() => {
   /*
   Message:
   This path file recommends the use of a custom field image. Would you like to download and install it?
@@ -169,104 +369,15 @@ export const AssetManagerModal = observer(() => {
   Message:
   If the url is provided, the image will be downloaded and cached in your computer.
   The url will also be included in the path file. The image will be automatically downloaded when the path file is opened on a different computer.
-  // The url will also be included in the path file. The image will be automatically downloaded when you open the path file on a different computer.
-  // The url will also be included in the path file. It will automatically download the image when you open the path file on a different computer.
-  // The url will also be included in the path file. It will automatically download the image when you open the path file on a different computer.
-  // The url will also be included in the path file that uses this image to allow other users to download the image when they open the path file on their browser.
 
   If the file is uploaded, the image will be stored in your computer, but not embed in the path file.
   The image needs to be uploaded again if the path file is opened on a different computer.
-  // You need to upload the image again if you open the path file on a different computer.
-  // When the path file is shared with other users, they will need to upload the image themselves.
-  // When the path file is opened on a different browser/computer, the image ...
   */
 
   return (
     <Modal symbol={AssetManagerModalSymbol}>
       <Card id="asset-manager-modal" className="modal-container">
-        <Typography variant="h3" fontSize={18} gutterBottom>
-          Field Image
-        </Typography>
-        <Box id="field-image-assets">
-          <Box id="assets-list-add">
-            <Box id="assets-list">
-              {/* Using the SVG viewBox solution to allow the use of min-height */}
-              <svg viewBox="0 0 0.6 0.4"></svg>
-              <Box id="assets-list-content">
-                <List dense>
-                  {assetManager.assets.map(asset => (
-                    <FieldImageAssetItem key={asset.signature} variables={variables} asset={asset} />
-                  ))}
-                </List>
-              </Box>
-            </Box>
-            <Typography variant="h4" gutterBottom>
-              New image
-            </Typography>
-            <Box
-              sx={{
-                marginTop: "1em",
-                display: "flex",
-                gap: "12px",
-                flexWrap: "wrap",
-                alignItems: "center",
-                width: "100%"
-              }}>
-              <ObserverInput
-                label="Name"
-                getValue={() => variables.newName}
-                setValue={(value: string) => (variables.newName = value)}
-                isValidIntermediate={() => true}
-                isValidValue={value => value !== ""}
-                sx={{ flexGrow: 1 }}
-                onKeyDown={e => e.stopPropagation()}
-              />
-              <ObserverInput
-                label="Height (mm)"
-                getValue={() => variables.newHeightInMM + ""}
-                setValue={(value: string) => {
-                  const setValue = parseFormula(value, NumberUOL.parse)!.compute(UnitOfLength.Millimeter);
-                  variables.newHeightInMM = Math.max(100, setValue);
-                }}
-                isValidIntermediate={() => true}
-                isValidValue={(candidate: string) => parseFormula(candidate, NumberUOL.parse) !== null}
-                numeric
-                sx={{ width: "100px" }}
-                onKeyDown={e => e.stopPropagation()}
-              />
-            </Box>
-            <Box
-              sx={{
-                marginTop: "1em",
-                display: "flex",
-                gap: "12px",
-                flexWrap: "nowrap",
-                alignItems: "center",
-                width: "100%"
-              }}>
-              <ObserverInput
-                label="URL"
-                getValue={() => variables.newUrl}
-                setValue={(value: string) => (variables.newUrl = value)}
-                isValidIntermediate={() => true}
-                isValidValue={value => value !== ""} // TODO
-                sx={{ flexGrow: 1 }}
-                onKeyDown={e => e.stopPropagation()}
-              />
-              <Typography variant="body1" sx={{}}>
-                or
-              </Typography>
-              <MuiFileInput
-                sx={{ flexGrow: 1 }}
-                placeholder="File Upload"
-                value={variables.newUpload}
-                onChange={action((file: File | null) => (variables.newUpload = file))}
-                size="small"
-              />
-            </Box>
-          </Box>
-          {selected && <FieldImagePreview preview={selected} />}
-        </Box>
+        <FieldImageSection />
       </Card>
     </Modal>
   );
