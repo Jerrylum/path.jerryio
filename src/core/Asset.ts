4bba14fb2efd3bed89e045468833f4b21f060f1f
Jerrylum <serverofjerry@gmail.com>
2024-01-23 03:44:17 +0800

    :art: Improve field images quality

--- src/core/Asset.ts
+++ src/core/Asset.ts
@@ -13,8 +13,8 @@ import { Hash } from "fast-sha256";
 import { makeAutoObservable, makeObservable, observable } from "mobx";
 import { ValidateNumber, hex, makeId, TextEncoder } from "./Util";
 import localforage from "localforage";
-import builtInFieldImage2024 from "../static/field2024.png";
-import builtInFieldPerimeter from "../static/field-perimeter.png";
+import builtInFieldImage2024 from "../static/VRC-OverUnder-Default-TileColor66_71-2000x2000.png";
+import builtInFieldPerimeter from "../static/VRC-FieldPerimeter-TileColor66_71-2000x2000.png";
 
 export const DEFAULT_ACCEPT_FILE_EXT = [".png", ".jpg", ".jpeg", ".gif"] as const;
 
@@ -418,11 +418,12 @@ export class AssetManager {
 
 const builtInAssets: FieldImageAsset<FieldImageOriginType>[] = [
   // 3683 = 145*2.54*10 ~= 3676.528, the size of the field perimeter in Fusion 360
-  createBuiltInFieldImage("VRC 2024 - Over Under", 3683, builtInFieldImage2024),
-  createBuiltInFieldImage("VRC Field Perimeter", 3683, builtInFieldPerimeter),
-  // 1912 is the size of the field perimeter in Fusion 360
-  createBuiltInFieldImage("VIQC 2024 - Full Volume", 1912, "/static/VIQC-2024-FullVolume-2000x1516.png"),
-  createBuiltInFieldImage("VIQC Field Perimeter 8ft×6ft", 1912, "/static/VIQC-FieldPerimeter-8ft6ft-2000x1516.png")
+  createBuiltInFieldImage("VRC 2024 - Over Under", 3690, builtInFieldImage2024),
+  createBuiltInFieldImage("VRC 2024 - Over Under (Skill)", 3690, "/static/VRC-OverUnder-Skill-TileColor66_71-2000x2000.png"),
+  createBuiltInFieldImage("VRC 2024 - Over Under (VEX U)", 3690, "/static/VRC-OverUnder-VEXU-TileColor66_71-2000x2000.png"),
+  createBuiltInFieldImage("VRC Field Perimeter", 3690, builtInFieldPerimeter),
+  createBuiltInFieldImage("VIQC 2024 - Full Volume", 1920, "/static/VIQC-FullVolume-8ft6ft-2000x1517.png"),
+  createBuiltInFieldImage("VIQC Field Perimeter 8ft×6ft", 1920, "/static/VIQC-FieldPerimeter-8ft6ft-2000x1517.png")
 ];
 
 export function getDefaultBuiltInFieldImage(): FieldImageAsset<FieldImageOriginType> {
