96a29e39bf70a2cf047472da92b9b8fac46adf0a
Nikhil Ramanuja <Nikhil.ramanuja.8@gmail.com>
2024-04-26 20:04:11 -0700

    realtime code output

--- src/app/Layouts.tsx
+++ src/app/Layouts.tsx
@@ -4,10 +4,16 @@ import { ControlAccordion } from "./common.blocks/panel/ControlAccordion";
 import { PanelContainer } from "./common.blocks/panel/Panel";
 import { GeneralConfigAccordion } from "./common.blocks/panel/GeneralConfigAccordion";
 import { PathConfigAccordion } from "./common.blocks/panel/PathAccordion";
+import { CodeAccordion } from "./common.blocks/panel/CodeAccordion";
 
 export const LayoutContext = React.createContext<LayoutType>(LayoutType.Classic);
 export const LayoutProvider = LayoutContext.Provider;
 
 export const getAllPanelContainers = (layout: LayoutType): PanelContainer[] => {
-  return [GeneralConfigAccordion({ layout }), ControlAccordion({ layout }), PathConfigAccordion({ layout })];
+  return [
+    GeneralConfigAccordion({ layout }),
+    CodeAccordion({ layout }),
+    ControlAccordion({ layout }),
+    PathConfigAccordion({ layout })
+  ];
 };
